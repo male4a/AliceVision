ab29ae37f8ab58c00dcabce0658ea45156b9d8fd
Fabien Servant <fabien.servant@technicolor.com>
2024-11-12 09:35:06 +0100

    add cost functions

--- src/aliceVision/sfm/bundle/costfunctions/intrinsicsProject.hpp
+++ src/aliceVision/sfm/bundle/costfunctions/intrinsicsProject.hpp
@@ -7,6 +7,7 @@
 #include <aliceVision/geometry/lie.hpp>
 #include <Eigen/Core>
 #include <ceres/ceres.h>
+#pragma once
 
 namespace aliceVision {
 namespace sfm {
@@ -32,8 +33,13 @@ class CostIntrinsicsProject : public ceres::CostFunction
         const Eigen::Map<const Vec3> pt(parameter_point);
         const Vec4 pth = pt.homogeneous();
 
+        const Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+
+        //Here we assume the intrinsincs object has been externally updated
         Vec2 pt_est = _intrinsics->project(pth, true);
         const double scale = (_measured.getScale() > 1e-12) ? _measured.getScale() : 1.0;
+
+        auto test = std::dynamic_pointer_cast<camera::Pinhole>(_intrinsics);
 
         residuals[0] = (pt_est(0) - _measured.getX()) / scale;
         residuals[1] = (pt_est(1) - _measured.getY()) / scale;
@@ -48,23 +54,23 @@ class CostIntrinsicsProject : public ceres::CostFunction
 
         if (jacobians[0] != nullptr)
         {
-            Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> J(jacobians[1], 2, params_size);
+            Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> J(jacobians[0], 2, params_size);
 
-            //J = d_res_d_pt_est * _intrinsics->getDerivativeTransformProjectWrtParams(T, pth);
+            J = d_res_d_pt_est * _intrinsics->getDerivativeTransformProjectWrtParams(T, pth);
         }
 
         if (jacobians[1] != nullptr)
         {
-            Eigen::Map<Eigen::Matrix<double, 2, 3, Eigen::RowMajor>> J(jacobians[2]);
+            Eigen::Map<Eigen::Matrix<double, 2, 3, Eigen::RowMajor>> J(jacobians[1]);
 
-            //J = d_res_d_pt_est * _intrinsics->getDerivativeTransformProjectWrtPoint3(T, pth);
+            J = d_res_d_pt_est * _intrinsics->getDerivativeTransformProjectWrtPoint3(T, pth);
         }
 
         return true;
     }
 
   private:
-    const sfmData::Observation& _measured;
+    const sfmData::Observation _measured;
     const std::shared_ptr<camera::IntrinsicBase> _intrinsics;
 };
 
